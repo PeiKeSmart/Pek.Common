4d31515e80f4722f6895290206821291104a8dcd
猿人易 <qcjxberin@outlook.com>
2024-08-29 14:42:34 +0800

    新增 Base64Helper 类及其方法
    添加了 `System.Text` 命名空间的引用。
    在 `Pek.Common.Helpers` 命名空间下新增了一个 `Base64Helper` 类。
    `Base64Helper` 类中新增了以下方法：
    - `Base64ToString(string strbase, Encoding encoding)`：将Base64编码解析成字符串，支持指定字符编码方案。
    - `Base64ToString(string strbase)`：将Base64编码解析成字符串，使用默认的UTF-8编码。
    - `Base64ToBytes(string strbase)`：将Base64编码解析成字节数组。
    - `StringToBase64(byte[] bytebase)`：将字节数组编码为Base64字符串。
    - `StringToBase64(string str, Encoding encoding)`：将字符串转为Base64编码，支持指定字符编码方案。
    - `StringToBase64(string str)`：将字符串转为Base64编码，使用默认的UTF-8编码。

---
+++ Pek.Common/Helpers/Base64Helper.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace Pek.Common.Helpers;
+
+/// <summary>
+/// Base64帮助类
+/// </summary>
+public class Base64Helper
+{
+    /// <summary>
+    /// 将Base64编码解析成字符串
+    /// </summary>
+    /// <param name="strbase">要解码的string字符</param>
+    /// <param name="encoding">字符编码方案</param>
+    /// <returns></returns>
+    public static string Base64ToString(string strbase, Encoding encoding)
+    {
+        var buff = Convert.FromBase64String(strbase);
+        return encoding.GetString(buff);
+    }
+
+    /// <summary>
+    /// 将Base64编码解析成字符串
+    /// </summary>
+    /// <param name="strbase">要解码的string字符</param>
+    /// <returns></returns>
+    public static string Base64ToString(string strbase)
+    {
+        return Base64ToString(strbase, Encoding.UTF8);
+    }
+
+    /// <summary>
+    /// 将Base64编码解析成字节数组
+    /// </summary>
+    /// <param name="strbase">要解码的string字符</param>
+    /// <returns></returns>
+    public static byte[] Base64ToBytes(string strbase)
+    {
+        return Convert.FromBase64String(strbase);
+    }
+
+    /// <summary>
+    /// 将字节数组为Base64编码
+    /// </summary>
+    /// <param name="bytebase">要编码的byte[]</param>
+    /// <returns></returns>
+    public static string StringToBase64(byte[] bytebase)
+    {
+        return Convert.ToBase64String(bytebase);
+    }
+
+    /// <summary>
+    /// 将字符串转为Base64编码
+    /// </summary>
+    /// <param name="str">要编码的string字符</param>
+    /// <param name="encoding">字符编码方案</param>
+    /// <returns></returns>
+    public static string StringToBase64(string str, Encoding encoding)
+    {
+        var buff = encoding.GetBytes(str);
+        return Convert.ToBase64String(buff);
+    }
+
+    /// <summary>
+    /// 将字符串转为Base64编码
+    /// </summary>
+    /// <param name="str">要编码的string字符</param>
+    /// <returns></returns>
+    public static string StringToBase64(string str)
+    {
+        var buff = Encoding.UTF8.GetBytes(str);
+        return Convert.ToBase64String(buff);
+    }
+}
