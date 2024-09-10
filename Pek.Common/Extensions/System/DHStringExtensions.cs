442791be0284af301fa1239c742eee452c4297cf
猿人易 <qcjxberin@outlook.com>
2024-09-10 17:31:47 +0800

    添加字符串和集合扩展方法
    在 `DHStringExtensions.cs` 文件中：
    * 添加了 `using NewLife;` 语句。
    * 添加了 `namespace System;` 命名空间。
    * 添加了 `DHStringExtensions` 静态类及其扩展方法：
      * `EnsureEndsWith` 方法：如果给定字符串的结尾不以指定字符结尾，则将字符添加到该字符串的结尾。
      * `EnsureStartsWith` 方法：如果给定字符串的开头不以指定字符开头，则将字符添加到该字符串的开头。
    
    在 `DHEnumerableExtensions.cs` 文件中：
    * 添加了 `namespace System.Collections.Generic;` 命名空间。
    * 添加了 `DHEnumerableExtensions` 静态类及其扩展方法：
      * `JoinAsString` 方法（两个重载）：将 `IEnumerable` 集合的成员串联为一个字符串，在每个成员之间使用指定的分隔符。
      * `WhereIf` 方法（两个重载）：根据给定条件，通过谓词过滤 `IEnumerable` 集合。

---
+++ Pek.Common/Extensions/System/DHStringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using NewLife;
+
+namespace System;
+
+/// <summary>
+/// 字符串(<see cref="string"/>) 扩展
+/// </summary>
+public static partial class DHStringExtensions
+{
+    /// <summary>
+    /// 如果给定字符串的结尾不以字符结尾，则将字符添加到该字符串的结尾。
+    /// </summary>
+    public static string EnsureEndsWith(this string str, char c, StringComparison comparisonType = StringComparison.Ordinal)
+    {
+        if (str.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(str));
+
+        if (str.EndsWith(c.ToString(), comparisonType))
+        {
+            return str;
+        }
+
+        return str + c;
+    }
+
+    /// <summary>
+    /// 如果给定字符串的开头不以字符开头，则将字符添加到该字符串的开头。
+    /// </summary>
+    public static string EnsureStartsWith(this string str, char c, StringComparison comparisonType = StringComparison.Ordinal)
+    {
+        if (str.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(str));
+
+        if (str.StartsWith(c.ToString(), comparisonType))
+        {
+            return str;
+        }
+
+        return c + str;
+    }
+}
