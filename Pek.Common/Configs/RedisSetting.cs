6ce259e31295cbe67f59392d35a8760b68eedda3
猿人易 <qcjxberin@outlook.com>
2024-09-19 11:24:56 +0800

    新增 RedisSetting 类及相关配置属性
    在 `RedisSetting.cs` 文件中：
    - 添加了 `System.ComponentModel` 和 `NewLife.Configuration` 的 `using` 语句。
    - 新增 `Pek.Configs` 命名空间。
    - 新增 `RedisSetting` 类，继承自 `Config<RedisSetting>`。
    - 添加 `CacheKeyPrefix` 和 `RedisEnabled` 属性，带有描述属性。
    - 为 `RedisSetting` 类添加 `DisplayName` 和 `Config` 特性。

---
+++ Pek.Common/Configs/RedisSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace Pek.Configs;
+
+/// <summary>工具配置</summary>
+[DisplayName("Redis配置")]
+//[XmlConfigFile("Config/DHUtil.config", 10_000)]
+[Config("RedisSetting")]
+public class RedisSetting : Config<RedisSetting>
+{
+    /// <summary>
+    /// Cache键前缀
+    /// </summary>
+    [Description("Cache键前缀")]
+    public String CacheKeyPrefix { get; set; } = "";
+
+    /// <summary>
+    /// 是否应该使用Redis服务
+    /// </summary>
+    [Description("是否应该使用Redis服务")]
+    public Boolean RedisEnabled { get; set; }
+}
