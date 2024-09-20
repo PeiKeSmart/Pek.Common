d2f616a75719fd86d12beb74a109b4b6a2a40b57
猿人易 <qcjxberin@outlook.com>
2024-09-20 15:51:08 +0800

    添加多个扩展方法和类型转换方法
    在 `DHExtensions.Common.cs` 文件中：
    * 添加了 `Pek` 命名空间。
    * 添加了 `DHExtensions` 静态类，并在其中添加了获取枚举值的扩展方法 `Value` 和泛型版本的 `Value<TResult>`。
    
    在 `DHExtensions.Validate.cs` 文件中：
    * 添加了 `System.Collections`、`System.Text` 和 `System.Text.RegularExpressions` 的 using 语句。
    * 添加了 `Pek` 命名空间。
    * 添加了 `DHExtensions` 静态类，并在其中添加了多个验证和检查方法：
      * `QuickValidate` 方法用于快速验证字符串是否符合指定的正则表达式。
      * `CheckNull` 方法用于检查对象是否为 null。
      * `IsEmpty` 方法用于判断字符串、Guid、StringBuilder、迭代集合和字典是否为空。
      * `StrIsNullOrEmpty` 方法用于判断字符串是否为 null 或空。
      * `IsNotNullAndWhiteSpace` 方法用于判断字符串是否不为 null 且不为空白。
      * `ObjIsNull` 方法用于判断对象是否为空。
      * `IsIPAddress`、`IsIP` 和 `IsIPSect` 方法用于判断字符串是否为 IP 地址格式。
    
    在 `Common.cs` 文件中：
    * 添加了 `Pek.Helpers` 命名空间。
    * 添加了 `Common` 静态类，并在其中添加了获取类型的扩展方法 `GetType` 和泛型版本的 `GetType<T>`。
    
    在 `Conv.cs` 文件中：
    * 添加了多个类型转换方法，包括将对象转换为 `byte`、`char`、`short`、`int`、`long`、`float`、`double`、`decimal`、`bool`、`DateTime`、`Guid` 以及泛型集合和枚举类型的方法。
    * 添加了一个将 IP 地址转换为 `long` 类型的方法 `ConvertIPToLong`。
    
    在 `Enum.cs` 文件中：
    * 添加了一个新的命名空间 `Pek.Helpers`。
    * 定义了一个静态类 `Enum`，其中包含了两个方法：`Parse` 和 `GetValue`。
      * `Parse` 方法用于将成员名或值转换为枚举实例。
      * `GetValue` 方法用于获取枚举成员的值。

---
+++ Pek.Common/Extensions/Common/DHExtensions.Common.cs
@@ -0,0 +1,24 @@
+﻿namespace Pek;
+
+/// <summary>
+/// 系统扩展 - 公共扩展
+/// </summary>
+public static partial class DHExtensions
+{
+    #region Value(获取枚举值)
+
+    /// <summary>
+    /// 获取枚举值
+    /// </summary>
+    /// <param name="instance">枚举实例</param>
+    public static int Value(this Enum instance) => Helpers.Enum.GetValue(instance.GetType(), instance);
+
+    /// <summary>
+    /// 获取枚举值
+    /// </summary>
+    /// <typeparam name="TResult">返回值类型</typeparam>
+    /// <param name="instance">枚举实例</param>
+    public static TResult Value<TResult>(this Enum instance) => Helpers.Conv.To<TResult>(instance.Value());
+
+    #endregion
+}
---
+++ Pek.Common/Helpers/Enum.cs
@@ -0,0 +1,59 @@
+﻿namespace Pek.Helpers;
+
+/// <summary>
+/// 枚举 操作
+/// </summary>
+public static partial class Enum
+{
+    /// <summary>
+    /// 枚举值字段
+    /// </summary>
+    private const string EnumValueField = "value__";
+
+    #region Parse(获取实例)
+
+    /// <summary>
+    /// 获取实例
+    /// </summary>
+    /// <typeparam name="TEnum">枚举类型</typeparam>
+    /// <param name="member">成员名或值，范例：Enum1枚举有成员A=0，则传入"A"或"0"获取 Enum1.A</param>
+    public static TEnum Parse<TEnum>(object member)
+    {
+        var value = member.SafeString();
+        if (value.IsEmpty())
+        {
+            if (typeof(TEnum).IsGenericType)
+                return default;
+            throw new ArgumentNullException(nameof(member));
+        }
+        return (TEnum)System.Enum.Parse(Common.GetType<TEnum>(), value, true);
+    }
+
+    #endregion
+
+    #region GetValue(获取成员值)
+
+    /// <summary>
+    /// 获取成员值
+    /// </summary>
+    /// <typeparam name="TEnum">枚举类型</typeparam>
+    /// <param name="member">成员名、值、实例均可，范例:Enum1枚举有成员A=0,可传入"A"、0、Enum1.A，获取值0</param>
+    /// <exception cref="ArgumentNullException">成员为空</exception>
+    public static int GetValue<TEnum>(object member) => GetValue(Common.GetType<TEnum>(), member);
+
+    /// <summary>
+    /// 获取成员值
+    /// </summary>
+    /// <param name="type">枚举类型</param>
+    /// <param name="member">成员名、值、实例均可，范例:Enum1枚举有成员A=0,可传入"A"、0、Enum1.A，获取值0</param>
+    /// <exception cref="ArgumentNullException">成员为空</exception>
+    public static int GetValue(Type type, object member)
+    {
+        string value = member.SafeString();
+        if (value.IsEmpty())
+            throw new ArgumentNullException(nameof(member));
+        return (int)System.Enum.Parse(type, member.ToString(), true);
+    }
+
+    #endregion
+}
