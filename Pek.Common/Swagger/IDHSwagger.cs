32672eb1f77ccdcc2d38158d18b9e9f688e60d99
猿人易 <qcjxberin@outlook.com>
2024-09-23 17:36:44 +0800

    添加 Pek.Swagger 命名空间及相关枚举和接口
    在 `ApiVersions.cs` 文件中，添加了一个新的命名空间 `Pek.Swagger`，并定义了一个枚举 `ApiVersions`，用于表示不同版本的 API。该枚举包含从 V1 到 V10 的版本，并为每个版本提供了中文注释说明。
    
    在 `IDHSwagger.cs` 文件中，添加了一个新的命名空间 `Pek.Swagger`，并定义了一个接口 `IDHSwagger`。该接口包含两个属性：`ApiVersions` 和 `Description`，分别用于表示接口版本和版本说明，并且都带有中文注释。

---
+++ Pek.Common/Swagger/IDHSwagger.cs
@@ -0,0 +1,17 @@
+﻿namespace Pek.Swagger;
+
+/// <summary>
+/// 表示基于Swagger的接口
+/// </summary>
+public interface IDHSwagger
+{
+    /// <summary>
+    /// 接口版本
+    /// </summary>
+    ApiVersions ApiVersions { get; set; }
+
+    /// <summary>
+    /// 版本说明
+    /// </summary>
+    String Description { get; set; }
+}
