5c3a1999051766ac48b935298d27ff29648d0010
猿人易 <qcjxberin@outlook.com>
2024-09-24 09:31:19 +0800

    添加 CloneHelper 以实现对象深度克隆
    在 `Pek.Common.csproj` 文件中添加了对 `System.Text.Json` 版本 `8.0.4` 的引用。
    
    新增了 `CloneHelper.cs` 文件，包含以下更改：
    - 引入 `System.Text.Json` 命名空间。
    - 定义 `Pek.Helpers` 命名空间。
    - 创建静态类 `CloneHelper`，用于深度克隆对象。
    - 在 `CloneHelper` 类中定义扩展方法 `DeepClone<T>`，使用 `JsonSerializer` 进行序列化和反序列化以实现深度克隆。
    - 使用 `ReferenceHandler.Preserve` 选项处理循环引用。

---
+++ Pek.Common/Helpers/CloneHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Pek.Helpers;
+
+/// <summary>
+/// 深度克隆辅助类
+/// </summary>
+public static class CloneHelper
+{
+    /// <summary>
+    /// 深度克隆
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static T DeepClone<T>(this T source)
+    {
+        // 不要序列化 null 对象，只需返回该对象的默认值
+        if (source == null)
+        {
+            return default(T);
+        }
+
+        var jsonString = JsonSerializer.Serialize(source, new JsonSerializerOptions // 将对象序列化为 JSON 字符串
+        {
+            ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve  // 选项会保留对象的引用信息，以便在反序列化时能够正确处理循环引用
+        });
+
+        return JsonSerializer.Deserialize<T>(jsonString);  // 将 JSON 字符串反序列化为对象
+    }
+}
