6e1b26d841c60b4ee18b7118da3ebedc769f16ed
猿人易 <qcjxberin@outlook.com>
2024-10-26 17:40:09 +0800

    添加多种实用方法和改进代码可读性
    在 DHExtensions.Common.cs 文件中：
    - 添加了多个 using 引用。
    - 添加 SafeValue、Description、GetMatch、GetMatchingValues 和 GetMatches 方法。
    
    在 Enum.cs 文件中：
    - 添加了多个 using 引用。
    - 修改 GetValue 方法的变量类型。
    - 添加 ParseByDescription、GetName、GetNames、GetDescription、GetDictionary 和 GetMemberInfos 方法。
    
    在 Reflection.cs 文件中：
    - 添加了多个 using 引用。
    - 新增 Reflection 类及其相关方法。
    
    在 Encrypt.cs 文件中：
    - 构造函数参数重命名以提高可读性。
    - 新增 DecryptData 方法。
    
    在 RsaHelper.cs 文件中：
    - 添加 System.Security.Cryptography 和 System.Text 引用。
    - 新增 RsaHelper 类及其相关方法。
    
    在 UnixTime.cs 文件中：
    - 新增 UnixTime 静态类及其相关方法。

--- Pek.Common/Extensions/Common/DHExtensions.Common.cs
+++ Pek.Common/Extensions/Common/DHExtensions.Common.cs
@@ -1,4 +1,7 @@
-﻿namespace Pek;
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Pek;
 
 /// <summary>
 /// 系统扩展 - 公共扩展
@@ -6,7 +9,6 @@
 public static partial class DHExtensions
 {
     #region Value(获取枚举值)
-
     /// <summary>
     /// 获取枚举值
     /// </summary>
@@ -19,6 +21,72 @@ public static partial class DHExtensions
     /// <typeparam name="TResult">返回值类型</typeparam>
     /// <param name="instance">枚举实例</param>
     public static TResult Value<TResult>(this Enum instance) => Helpers.Conv.To<TResult>(instance.Value());
+    #endregion
+
+    #region SafeValue(安全获取值)
+    /// <summary>
+    /// 安全获取值，当值为null时，不会抛出异常
+    /// </summary>
+    /// <typeparam name="T">数据类型</typeparam>
+    /// <param name="value">可空值</param>
+    public static T SafeValue<T>(this T? value) where T : struct => value ?? default;
+    #endregion
+
+    #region Description(获取枚举描述)
+
+    /// <summary>
+    /// 获取枚举描述，使用<see cref="DescriptionAttribute"/>特性设置描述
+    /// </summary>
+    /// <param name="instance">枚举实例</param>
+    public static string Description(this Enum instance) => Helpers.Enum.GetDescription(instance.GetType(), instance);
+
+    #endregion
+
+    #region GetMatch(获取匹配项)
+
+    /// <summary>
+    /// 在指定的输入字符串中搜索指定的正则表达式的第一个匹配项
+    /// </summary>
+    /// <param name="value">要搜索匹配项的字符串</param>
+    /// <param name="pattern">要匹配的正则表达式模式</param>
+    /// <returns>一个对象，包含有关匹配项的信息</returns>
+    public static string GetMatch(this string value, string pattern)
+    {
+        if (value.IsEmpty())
+            return string.Empty;
+        return Regex.Match(value, pattern).Value;
+    }
+
+    /// <summary>
+    /// 在指定的输入字符串中搜索指定的正则表达式的所有匹配项的字符串集合
+    /// </summary>
+    /// <param name="value">要搜索匹配项的字符串</param>
+    /// <param name="pattern">要匹配的正则表达式模式</param>
+    /// <returns> 一个集合，包含有关匹配项的字符串值</returns>
+    public static IEnumerable<string> GetMatchingValues(this string value, string pattern)
+    {
+        if (value.IsEmpty())
+            return new string[] { };
+        return value.GetMatchingValues(pattern, RegexOptions.None);
+    }
+
+    /// <summary>
+    /// 使用正则表达式来确定一个给定的正则表达式模式的所有匹配的字符串返回的枚举
+    /// </summary>
+    /// <param name="value">输入字符串</param>
+    /// <param name="pattern">正则表达式</param>
+    /// <param name="options">比较规则</param>
+    /// <returns>匹配字符串的枚举</returns>
+    public static IEnumerable<string> GetMatchingValues(this string value, string pattern, RegexOptions options) =>
+        from Match match in value.GetMatches(pattern, options) where match.Success select match.Value;
+
+    /// <summary>
+    /// 使用正则表达式来确定指定的正则表达式模式的所有匹配项
+    /// </summary>
+    /// <param name="value">值</param>
+    /// <param name="pattern">正则表达式</param>
+    /// <param name="options">比较规则</param>
+    public static MatchCollection GetMatches(this string value, string pattern, RegexOptions options) => Regex.Matches(value, pattern, options);
 
     #endregion
 }
