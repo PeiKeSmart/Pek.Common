4c2a5e9aaa8058fffec2bb02c9bab9334b071ab3
猿人易 <qcjxberin@outlook.com>
2024-11-07 19:40:37 +0800

    启用可空引用类型并新增 IPekLanguage 接口
    在 `Pek.Common.csproj` 文件中添加 `<Nullable>enable</Nullable>` 配置，以启用可空引用类型的支持。
    新增 `IPekLanguage.cs` 文件，定义了 `Pek` 命名空间下的 `IPekLanguage` 接口。该接口包含一个 `Translate` 方法，用于翻译内容，接受一个字符串参数 `key` 并返回一个可空的字符串。

---
+++ Pek.Common/Localization/IPekLanguage.cs
@@ -0,0 +1,14 @@
+﻿namespace Pek;
+
+/// <summary>
+/// 翻译语言接口
+/// </summary>
+public interface IPekLanguage
+{
+    /// <summary>
+    /// 翻译内容
+    /// </summary>
+    /// <param name="key">要翻译的内容</param>
+    /// <returns></returns>
+    String? Translate(String key);
+}
