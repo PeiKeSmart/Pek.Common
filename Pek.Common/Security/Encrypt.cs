5507fda038af8a92641d647323ce13840aa5cc55
猿人易 <qcjxberin@outlook.com>
2024-11-07 16:51:09 +0800

    添加 GetSha1 方法用于 SHA-1 加密字符串
    添加了一个新的方法 `GetSha1`，用于采用 SHA-1 算法加密字符串（小写）。
    该方法接收一个需要加密的字符串 `encypStr` 作为参数。
    使用 `SHA1.Create()` 创建一个 SHA-1 算法的实例。
    通过 `sha1.ComputeHash` 方法计算字符串的哈希值。
    使用 `StringBuilder` 将哈希值转换为小写的十六进制字符串。
    返回加密后的字符串。

--- Pek.Common/Security/Encrypt.cs
+++ Pek.Common/Security/Encrypt.cs
@@ -643,6 +643,24 @@ public static partial class Encrypt
         }
     }
 
+    /// <summary>
+    /// 采用SHA-1算法加密字符串（小写）
+    /// </summary>
+    /// <param name="encypStr">需要加密的字符串</param>
+    /// <returns></returns>
+    public static string GetSha1(string encypStr)
+    {
+        var sha1 = SHA1.Create();
+        var sha1Arr = sha1.ComputeHash(Encoding.UTF8.GetBytes(encypStr));
+        var enText = new StringBuilder();
+        foreach (var b in sha1Arr)
+        {
+            enText.AppendFormat("{0:x2}", b);
+        }
+
+        return enText.ToString();
+    }
+
     #endregion
 
     #region SHA256加密
