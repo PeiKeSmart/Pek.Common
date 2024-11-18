db900c84317f39c7878868ecce3e56a05b8691be
猿人易 <qcjxberin@outlook.com>
2024-11-18 16:36:47 +0800

    添加基于 Base32 编码的 ID 生成工具类
    在 `CorrelationIdGenerator.cs` 文件中添加了一个新的命名空间 `Pek.Ids`，并定义了一个静态类 `CorrelationIdGenerator`。该类包含一个用于生成基于 Base32 编码的唯一 ID 的方法 `GetNextId` 和一个私有方法 `GenerateId`。这些代码仅在 `NETSTANDARD2_1_OR_GREATER` 或 `NET8_0_OR_GREATER` 的条件下编译。
    
    在 `FastGuid.cs` 文件中添加了一个新的命名空间 `Pek.Ids`，并定义了一个密封类 `FastGuid`。该类包含一个静态方法 `NewGuid` 用于生成新的 `FastGuid` 实例，以及一个私有方法 `GenerateGuidString` 用于生成基于 Base32 编码的 GUID 字符串。这些代码仅在 `NETSTANDARD2_1_OR_GREATER` 或 `NET8_0_OR_GREATER` 的条件下编译。
    
    在 `IdHelper.cs` 文件中添加了对 `NewLife.Data` 的引用，并定义了一个新的命名空间 `Pek.Ids`。该文件中新增了一个静态类 `IdHelper`，包含多个方法用于生成不同类型的 ID，包括 `GetNextId` 和 `GetIdString` 方法，这些方法分别调用 `CorrelationIdGenerator` 和 `FastGuid` 类中的方法来生成 ID 字符串。这些代码仅在 `NETSTANDARD2_1_OR_GREATER` 或 `NET8_0_OR_GREATER` 的条件下编译。

---
+++ Pek.Common/Ids/FastGuid.cs
@@ -0,0 +1,73 @@
+﻿#if NETSTANDARD2_1_OR_GREATER || NET8_0_OR_GREATER
+
+namespace Pek.Ids;
+
+public sealed class FastGuid
+{
+    // Base32 encoding - in ascii sort order for easy text based sorting
+    private static readonly Char[] s_encode32Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUV".ToCharArray();
+    // Global ID
+    private static Int64 NextId = InitializeNextId();
+
+    // Instance components
+    private String? _idString;
+
+    internal Int64 IdValue { get; }
+
+    public String IdString
+    {
+        get
+        {
+            if (_idString == null)
+            {
+                _idString = GenerateGuidString(this);
+            }
+            return _idString;
+        }
+    }
+
+    // Static constructor to initialize global components
+    private static Int64 InitializeNextId()
+    {
+        var guidBytes = Guid.NewGuid().ToByteArray();
+
+        // Use the first 4 bytes from the Guid to initialize global ID
+        return
+            guidBytes[0] << 32 |
+            guidBytes[1] << 40 |
+            guidBytes[2] << 48 |
+            guidBytes[3] << 56;
+    }
+
+    internal FastGuid(long id)
+    {
+        IdValue = id;
+    }
+
+    public static FastGuid NewGuid()
+    {
+        return new FastGuid(Interlocked.Increment(ref NextId));
+    }
+
+    private static String GenerateGuidString(FastGuid guid)
+    {
+        return String.Create(13, guid.IdValue, (buffer, value) =>
+        {
+            var encode32Chars = s_encode32Chars;
+            buffer[12] = encode32Chars[value & 31];
+            buffer[11] = encode32Chars[(value >> 5) & 31];
+            buffer[10] = encode32Chars[(value >> 10) & 31];
+            buffer[9] = encode32Chars[(value >> 15) & 31];
+            buffer[8] = encode32Chars[(value >> 20) & 31];
+            buffer[7] = encode32Chars[(value >> 25) & 31];
+            buffer[6] = encode32Chars[(value >> 30) & 31];
+            buffer[5] = encode32Chars[(value >> 35) & 31];
+            buffer[4] = encode32Chars[(value >> 40) & 31];
+            buffer[3] = encode32Chars[(value >> 45) & 31];
+            buffer[2] = encode32Chars[(value >> 50) & 31];
+            buffer[1] = encode32Chars[(value >> 55) & 31];
+            buffer[0] = encode32Chars[(value >> 60) & 31];
+        });
+    }
+}
+#endif
---
+++ Pek.Common/Ids/IdHelper.cs
@@ -0,0 +1,50 @@
+﻿using NewLife.Data;
+
+namespace Pek.Ids;
+
+/// <summary>
+/// Id 生成器
+/// </summary>
+public static class IdHelper
+{
+    public static readonly Snowflake snowflake = new Snowflake();
+
+#if NETSTANDARD2_1_OR_GREATER || NET8_0_OR_GREATER
+    /// <summary>
+    /// 微软获取新的13位Id字符串
+    /// </summary>
+    /// <example>0HLV413GIHKK5</example>
+    /// <returns></returns>
+    public static String GetNextId() => CorrelationIdGenerator.GetNextId();
+
+    /// <summary>
+    /// 微软获取Id的字符串方法
+    /// </summary>
+    /// <returns></returns>
+    public static String GetIdString() => FastGuid.NewGuid().IdString;
+#endif
+
+    /// <summary>
+    /// 生成sessionid
+    /// </summary>
+    /// <example>62acfda11f5a4b3c</example>
+    public static string GenerateSid()
+    {
+        var i = 1;
+        var byteArray = Guid.NewGuid().ToByteArray();
+        foreach (var b in byteArray)
+        {
+            i *= b + 1;
+        }
+        return string.Format("{0:x}", i - DateTime.Now.Ticks);
+    }
+
+    /// <summary>
+    /// 获取NewLife的改进雪花算法
+    /// </summary>
+    /// <returns></returns>
+    public static Int64 GetSId()
+    {
+        return snowflake.NewId();
+    }
+}
