bd22bf65a170cef6d251f6c17c36dadf8f07e705
猿人易 <qcjxberin@outlook.com>
2024-11-29 14:13:32 +0800

    添加对象扩展方法和表达式复制功能
    在 `Extensions.Object.As.cs` 文件中添加了 `Pek.Extensions` 命名空间，并新增了静态部分类 `ObjectExtensions`，包含多个扩展方法：
    - `As<T>`：将当前对象转换为指定类型。
    - `AsOrDefault<T>`：尝试将当前对象转换为指定类型，如果转换失败则返回默认值。
    - `AsOrDefault<T>(T defaultValue)`：尝试将当前对象转换为指定类型，如果转换失败则返回指定的默认值。
    - `AsOrDefault<T>(Func<T> defaultValueFactory)`：尝试将当前对象转换为指定类型，如果转换失败则调用默认值工厂函数并返回其结果。
    - `AsOrDefault<T>(Func<Object, T> defaultValueFactory)`：尝试将当前对象转换为指定类型，如果转换失败则调用带有当前对象参数的默认值工厂函数并返回其结果。
    
    在 `Extensions.Object.ExpressionCopier.cs` 文件中添加了 `Pek.Extensions` 命名空间，并新增了静态部分类 `ObjectExtensions`，包含扩展方法 `ExpressionCopy<T>`：
    - `ExpressionCopy<T>`：使用表达式树复制当前对象。
    
    在 `ObjectExtensions` 类中添加了一个私有的静态类 `Copier<T>`，用于实现对象的复制功能：
    - `Copy(T source)`：复制给定的数据源对象。
    - `GetAllPropertiesOrFields()`：获取所有属性或字段。
    
    在 `Copier<T>` 类中定义了一些私有的静态成员：
    - `_parameterExpression`：参数表达式。
    - `_check`：检查字典。
    - `_func`：用于复制的函数。

---
+++ Pek.Common/Extensions/Object/Extensions.Object.ExpressionCopier.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Pek.Extensions;
+
+/// <summary>
+/// 对象(<see cref="Object"/>) 扩展
+/// </summary>
+public static partial class ObjectExtensions
+{
+    /// <summary>
+    /// 表达式复制
+    /// </summary>
+    /// <typeparam name="T">对象类型</typeparam>
+    /// <param name="source">数据源</param>
+    public static T ExpressionCopy<T>(this T source) => Copier<T>.Copy(source);
+
+    /// <summary>
+    /// 复制器
+    /// </summary>
+    /// <typeparam name="T">对象类型</typeparam>
+    private static class Copier<T>
+    {
+        /// <summary>
+        /// 参数表达式
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        private static readonly ParameterExpression _parameterExpression = Expression.Parameter(typeof(T), "p");
+
+        /// <summary>
+        /// 检查字典
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once CollectionNeverUpdated.Local
+        // ReSharper disable once StaticMemberInGenericType
+        private static readonly Dictionary<String, Expression> _check = [];
+
+        /// <summary>
+        /// 函数
+        /// </summary>
+        private static Func<T, T>? _func;
+
+        /// <summary>
+        /// 复制
+        /// </summary>
+        /// <param name="source">数据源</param>
+        public static T Copy(T source)
+        {
+            if (_func == null)
+            {
+                var memberBindings = new List<MemberBinding>();
+                foreach (var item in GetAllPropertiesOrFields())
+                {
+                    if (_check.TryGetValue(item.Name, out var value))
+                    {
+                        var memberBinding = Expression.Bind(item, value);
+                        memberBindings.Add(memberBinding);
+                    }
+                    else
+                    {
+                        if (typeof(T).GetProperty(item.Name) != null || typeof(T).GetField(item.Name) != null)
+                        {
+                            var memberBinding = Expression.Bind(item,
+                                Expression.PropertyOrField(_parameterExpression, item.Name));
+                            memberBindings.Add(memberBinding);
+                        }
+                    }
+                }
+
+                var memberInitExpression =
+                    Expression.MemberInit(Expression.New(typeof(T)), [.. memberBindings]);
+                var lambda = Expression.Lambda<Func<T, T>>(memberInitExpression, _parameterExpression);
+                _func = lambda.Compile();
+            }
+            return _func.Invoke(source);
+        }
+
+        /// <summary>
+        /// 获取所有属性或字段
+        /// </summary>
+        private static IEnumerable<MemberInfo> GetAllPropertiesOrFields()
+        {
+            foreach (var item in typeof(T).GetProperties())
+                yield return item;
+            foreach (var item in typeof(T).GetFields())
+                yield return item;
+        }
+    }
+}
