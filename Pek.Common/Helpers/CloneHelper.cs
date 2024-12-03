f6e9320c43ee5725a6fe645308b92947206a3b2d
猿人易 <qcjxberin@outlook.com>
2024-12-03 15:07:39 +0800

    重命名方法并删除不再使用的类和方法
    将 `CloneHelper` 类中的 `DeepClone` 方法重命名为 `DeepCloneWithJson`。
    删除了 `ObjectToInferredTypesConverter` 类及其实现。
    删除了 `JsonElementExtensions` 类及其 `ToObject` 扩展方法。
    从 `_jsonSerializerOptions` 中移除了 `ObjectToInferredTypesConverter` 转换器。

--- Pek.Common/Helpers/CloneHelper.cs
+++ Pek.Common/Helpers/CloneHelper.cs
@@ -10,7 +10,6 @@ public static class CloneHelper {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
         ReferenceHandler = ReferenceHandler.Preserve,  // 选项会保留对象的引用信息，以便在反序列化时能够正确处理循环引用
-        Converters = { new ObjectToInferredTypesConverter() },
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull // 忽略 null 值
     };
 
@@ -20,7 +19,7 @@ public static class CloneHelper {
     /// <typeparam name="T"></typeparam>
     /// <param name="source"></param>
     /// <returns></returns>
-    public static T? DeepClone<T>(this T source)
+    public static T? DeepCloneWithJson<T>(this T source)
     {
         // 不要序列化 null 对象，只需返回该对象的默认值
         if (source == null)
@@ -31,55 +30,5 @@ public static class CloneHelper {
         var jsonString = JsonSerializer.Serialize(source, _jsonSerializerOptions); // 将对象序列化为 JSON 字符串
 
         return JsonSerializer.Deserialize<T>(jsonString, _jsonSerializerOptions);  // 将 JSON 字符串反序列化为对象
-    }
-}
-
-public class ObjectToInferredTypesConverter : JsonConverter<Object>
-{
-    public override Object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        using var doc = JsonDocument.ParseValue(ref reader);
-        return doc.RootElement.Clone().ToObject();
-    }
-
-    public override void Write(Utf8JsonWriter writer, Object value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, value.GetType(), options);
-}
-
-public static class JsonElementExtensions {
-    public static Object ToObject(this JsonElement element)
-    {
-        switch (element.ValueKind)
-        {
-            case JsonValueKind.Object:
-                var obj = new Dictionary<String, Object>();
-                foreach (var property in element.EnumerateObject())
-                {
-                    obj[property.Name] = property.Value.ToObject();
-                }
-                return obj;
-            case JsonValueKind.Array:
-                var array = new List<Object>();
-                foreach (var item in element.EnumerateArray())
-                {
-                    array.Add(item.ToObject());
-                }
-                return array;
-            case JsonValueKind.String:
-                return element.GetString()!;
-            case JsonValueKind.Number:
-                if (element.TryGetInt32(out var intValue))
-                    return intValue;
-                if (element.TryGetInt64(out var longValue))
-                    return longValue;
-                return element.GetDouble();
-            case JsonValueKind.True:
-                return true;
-            case JsonValueKind.False:
-                return false;
-            case JsonValueKind.Null:
-                return null!;
-            default:
-                throw new NotSupportedException($"Unsupported JsonValueKind: {element.ValueKind}");
-        }
     }
 }
