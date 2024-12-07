2d6dc79fb47e2e72417d436a90da054ffc8444bb
猿人易 <qcjxberin@outlook.com>
2024-12-07 14:05:07 +0800

    添加语法高亮和关键词排序功能
    添加了 `Highlighter.cs` 文件，定义了 `Highlighter` 类，用于语法高亮。该类包含规则列表、构造函数和高亮方法。还添加了静态方法 `FromExtension`，根据文件扩展名返回相应的高亮器。
    
    添加了 `HighlighterElement.cs` 文件，定义了 `HighlighterElement` 类，用于表示高亮元素。该类包含值和属性字典。
    
    添加了 `HighlighterRule.cs` 文件，定义了 `HighlighterRule` 类，用于表示高亮规则。该类实现了 `IDictionary<String, String>` 接口，包含正则表达式模式和属性字典。
    
    添加了 `KeywordSort.cs` 文件，定义了 `KeywordSort` 类，用于关键词排序。包含静态方法 `SortKeywords`，按关键词包含关系分组并排序。
    
    添加了 `SortComapre.cs` 文件，定义了 `SortComapre` 类，实现了 `IComparer` 接口，用于关键词排序比较。包含 `Compare` 方法，根据包含关系进行比较。

---
+++ Pek.Common/SyntaxHighlighing/HighlighterElement.cs
@@ -0,0 +1,13 @@
+﻿namespace Pek.SyntaxHighlighing;
+
+public class HighlighterElement
+{
+    public string Value { get; set; }
+    public Dictionary<String, String> Attributes { get; set; }
+
+    public HighlighterElement(String value, IDictionary<String, String> attributes)
+    {
+        Value = value;
+        Attributes = new Dictionary<String, String>(attributes);
+    }
+}
---
+++ Pek.Common/SyntaxHighlighing/HighlighterRule.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Pek.SyntaxHighlighing;
+
+public class HighlighterRule : IDictionary<String, String>
+{
+    public Regex Pattern { get; set; }
+    public Dictionary<String, String> Attributes { get; set; }
+
+    public ICollection<String> Keys => ((IDictionary<String, String>)Attributes).Keys;
+
+    public ICollection<String> Values => ((IDictionary<String, String>)Attributes).Values;
+
+    public Int32 Count => ((IDictionary<String, String>)Attributes).Count;
+
+    public Boolean IsReadOnly => ((IDictionary<String, String>)Attributes).IsReadOnly;
+
+    public String this[String key] { get => ((IDictionary<String, String>)Attributes)[key]; set => ((IDictionary<String, String>)Attributes)[key] = value; }
+
+    public HighlighterRule(Regex pattern)
+    {
+        Pattern = pattern;
+        Attributes = [];
+    }
+
+    public HighlighterRule(String patternString) : this(new Regex(patternString, RegexOptions.Compiled | RegexOptions.Singleline)) { }
+
+    public HighlighterRule(Regex pattern, IDictionary<String, String> attributes)
+    {
+        Pattern = pattern;
+        Attributes = new Dictionary<String, String>(attributes);
+    }
+
+    public HighlighterRule(String patternString, IDictionary<String, String> attributes)
+        : this(new Regex(patternString, RegexOptions.Compiled | RegexOptions.Singleline), attributes) { }
+
+    public void Add(String key, String value) => ((IDictionary<String, String>)Attributes).Add(key, value);
+
+    public Boolean ContainsKey(String key) => ((IDictionary<String, String>)Attributes).ContainsKey(key);
+
+    public Boolean Remove(String key) => ((IDictionary<String, String>)Attributes).Remove(key);
+
+    public Boolean TryGetValue(String key, out String value) => ((IDictionary<String, String>)Attributes).TryGetValue(key, out value!);
+
+    public void Add(KeyValuePair<String, String> item) => ((IDictionary<String, String>)Attributes).Add(item);
+
+    public void Clear() => ((IDictionary<String, String>)Attributes).Clear();
+
+    public Boolean Contains(KeyValuePair<String, String> item) => ((IDictionary<String, String>)Attributes).Contains(item);
+
+    public void CopyTo(KeyValuePair<String, String>[] array, Int32 arrayIndex) => ((IDictionary<String, String>)Attributes).CopyTo(array, arrayIndex);
+
+    public Boolean Remove(KeyValuePair<String, String> item) => ((IDictionary<String, String>)Attributes).Remove(item);
+
+    public IEnumerator<KeyValuePair<String, String>> GetEnumerator() => ((IDictionary<String, String>)Attributes).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IDictionary<String, String>)Attributes).GetEnumerator();
+}
