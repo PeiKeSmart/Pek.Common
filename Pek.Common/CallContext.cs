64ef97c922b071709bcb7be0537eb5ba54eb61e9
猿人易 <qcjxberin@outlook.com>
2024-12-19 20:10:03 +0800

    更新 DH.NCore 包版本并新增 CallContext 和 JTextAccessor 类
    更新了 `Pek.Common.csproj` 文件中的 `DH.NCore` 包版本，从 `4.1.2024.1213-beta0858` 更新到 `4.1.2024.1216-beta0150`。
    
    在 `CallContext.cs` 文件中：
    - 新增了 `System.Collections.Concurrent` 的引用。
    - 新增了 `Pek` 命名空间。
    - 新增了 `CallContext` 静态类，用于在线程内存储和获取唯一对象。
    - 新增了 `CallContext<T>` 泛型静态类，用于在线程内存储和获取特定类型的唯一对象。
    
    在 `JTextAccessor.cs` 文件中：
    - 新增了 `System.Dynamic` 和 `System.Text.Json` 的引用。
    - 新增了 `Pek` 命名空间。
    - 新增了 `JTextAccessor` 类，该类继承自 `DynamicObject`，用于通过动态访问 `System.Text.Json` 对象。
    - `JTextAccessor` 类包含一个私有的 `JsonElement` 字段 `_content`。
    - 重写了 `TryGetMember` 方法，用于动态获取 JSON 属性。
    - 新增了静态方法 `Obtain`，用于根据 `JsonElement` 的类型返回相应的值或对象。

---
+++ Pek.Common/CallContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace Pek;
+
+/// <summary>
+/// 取线程内唯一对象
+/// </summary>
+public static class CallContext
+{
+    static readonly ConcurrentDictionary<String, AsyncLocal<Object>> state = new();
+
+    public static void SetData(String name, Object data) =>
+        state.GetOrAdd(name, _ => new AsyncLocal<Object>()).Value = data;
+
+    public static Object? GetData(String name) =>
+        state.TryGetValue(name, out var data) ? data.Value : null;
+}
+
+/// <summary>
+/// 取线程内唯一对象
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public static class CallContext<T>
+{
+    static readonly ConcurrentDictionary<String, AsyncLocal<T>> state = new();
+
+    public static void SetData(String name, T data) => state.GetOrAdd(name, _ => new AsyncLocal<T>()).Value = data;
+
+    public static T? GetData(String name) => state.TryGetValue(name, out var data) ? data.Value : default;
+}
---
+++ Pek.Common/JTextAccessor.cs
@@ -0,0 +1,53 @@
+﻿using System.Dynamic;
+using System.Text.Json;
+
+namespace Pek;
+
+/// <summary>
+/// 通过Dynamic访问System.Text.Json对象
+/// 参考：https://blog.51cto.com/shanyou/3048736
+/// </summary>
+public class JTextAccessor(JsonElement content) : DynamicObject
+{
+    private readonly JsonElement _content = content;
+
+    public override Boolean TryGetMember(GetMemberBinder binder, out Object? result)
+    {
+        result = null;
+        if (_content.TryGetProperty(binder.Name, out var value))
+        {
+            result = JTextAccessor.Obtain(value);
+        }
+        else return false;
+        return true;
+    }
+
+    private static Object? Obtain(in JsonElement element)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.String: return element.GetString();
+            case JsonValueKind.Null: return null;
+            case JsonValueKind.False: return false;
+            case JsonValueKind.True: return true;
+            case JsonValueKind.Number: return element.GetDouble();
+            default: break;
+        }
+
+        if (element.ValueKind == JsonValueKind.Array)
+        {
+            var list = new List<Object>();
+            foreach (var item in element.EnumerateArray())
+            {
+                var m = Obtain(item);
+                if (m == null) continue;
+
+                list.Add(m);
+            }
+
+            return list;
+        }
+        // Undefine、Object
+        else return new JTextAccessor(element);
+    }
+}
