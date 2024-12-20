f7efd358cb4e5168d44993c2c83dfb1e3b1bbb42
猿人易 <qcjxberin@outlook.com>
2024-12-20 09:52:15 +0800

    添加 MailHelper 类及其 GetEmailSuffix 方法
    新增命名空间 Pek.Helpers 并添加 MailHelper 类。
    在 MailHelper 类中添加静态方法 GetEmailSuffix。
    GetEmailSuffix 方法用于获取电子邮件地址的后缀。
    方法会检查传入的电子邮件地址是否为空或仅包含空白字符，若是则抛出 ArgumentException 异常。
    方法会查找电子邮件地址中最后一个 '@' 符号的位置，若找不到或 '@' 符号在最后一个字符位置，则抛出 ArgumentException 异常。
    方法返回 '@' 符号之后的字符串部分，即电子邮件地址的后缀。

---
+++ Pek.Common/Helpers/MailHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Pek.Helpers;
+
+/// <summary>
+/// 邮件帮助类
+/// </summary>
+public class MailHelper
+{
+    public static String GetEmailSuffix(String email)
+    {
+        if (String.IsNullOrWhiteSpace(email))
+        {
+            throw new ArgumentException("Email address cannot be null or empty.", nameof(email));
+        }
+
+        var atIndex = email.LastIndexOf('@');
+        if (atIndex < 0 || atIndex == email.Length - 1)
+        {
+            throw new ArgumentException("Invalid email address format.", nameof(email));
+        }
+
+        return email[(atIndex + 1)..];
+    }
+}
