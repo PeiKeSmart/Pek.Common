0bc9f66df521d809d620e671c4d5a729bde54b24
猿人易 <qcjxberin@outlook.com>
2024-12-26 15:40:51 +0800

    添加 IDHStartup 接口及其方法和属性
    在 IDHStartup.cs 文件中添加了新的命名空间 Pek.VirtualFileSystem 和 Pek.Infrastructure。
    新增了一个接口 IDHStartup，用于在应用程序启动时配置服务和中间件。
    IDHStartup 接口包含以下方法和属性：
    - ConfigureVirtualFileSystem(DHVirtualFileSystemOptions options)：配置虚拟文件系统。
    - ConfigureArea()：将区域路由写入数据库。
    - ChangeMenu()：调整菜单。
    - Update()：升级处理逻辑。
    - StartupOrder：获取此启动配置实现的顺序。
    - ConfigureOrder：获取此启动配置实现的顺序，主要针对 ConfigureMiddleware、UseRouting 前执行的数据、UseAuthentication 或者 UseAuthorization 后面 Endpoints 前执行的数据。

---
+++ Pek.Common/Infrastructure/IDHStartup.cs
@@ -0,0 +1,40 @@
+﻿using Pek.VirtualFileSystem;
+
+namespace Pek.Infrastructure;
+
+/// <summary>
+/// 表示应用程序启动时配置服务和中间件的对象
+/// </summary>
+public interface IDHStartup
+{
+    /// <summary>
+    /// 配置虚拟文件系统
+    /// </summary>
+    /// <param name="options">虚拟文件配置</param>
+    void ConfigureVirtualFileSystem(DHVirtualFileSystemOptions options);
+
+    /// <summary>
+    /// 将区域路由写入数据库
+    /// </summary>
+    void ConfigureArea();
+
+    /// <summary>
+    /// 调整菜单
+    /// </summary>
+    void ChangeMenu();
+
+    /// <summary>
+    /// 升级处理逻辑
+    /// </summary>
+    void Update();
+
+    /// <summary>
+    /// 获取此启动配置实现的顺序
+    /// </summary>
+    Int32 StartupOrder { get; }
+
+    /// <summary>
+    /// 获取此启动配置实现的顺序。主要针对ConfigureMiddleware、UseRouting前执行的数据、UseAuthentication或者UseAuthorization后面 Endpoints前执行的数据
+    /// </summary>
+    Int32 ConfigureOrder { get; }
+}
