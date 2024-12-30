56005ef75da0c8fb25ad31104c3c033cb1c6dec5
猿人易 <qcjxberin@outlook.com>
2024-12-30 11:40:22 +0800

    添加Pek.Events命名空间及IEventPublisher接口方法
    添加了Pek.Events命名空间。在IEventPublisher接口中添加了XML注释，描述了接口的用途和方法。新增了PublishAsync方法用于异步发布事件给消费者，新增了Publish方法用于同步发布事件给消费者。

---
+++ Pek.Common/Events/IEventPublisher.cs
@@ -0,0 +1,22 @@
+﻿namespace Pek.Events;
+
+/// <summary>
+/// 表示事件发布者
+/// </summary>
+public partial interface IEventPublisher
+{
+    /// <summary>
+    /// 发布事件给消费者
+    /// </summary>
+    /// <typeparam name="TEvent">事件类型</typeparam>
+    /// <param name="event">事件对象</param>
+    /// <returns>一个表示异步操作的任务</returns>
+    Task PublishAsync<TEvent>(TEvent @event);
+
+    /// <summary>
+    /// 发布事件到消费者
+    /// </summary>
+    /// <typeparam name="TEvent">事件类型</typeparam>
+    /// <param name="event">事件对象</param>
+    void Publish<TEvent>(TEvent @event);
+}
