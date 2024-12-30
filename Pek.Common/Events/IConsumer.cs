e5c13028ed0513789ed77ca87f9b235d9d5e41a2
猿人易 <qcjxberin@outlook.com>
2024-12-30 11:22:11 +0800

    添加 IConsumer<T> 和 IConsumerBase 接口
    在 IConsumer.cs 文件中添加了 Pek.Events 命名空间，并添加了泛型接口 IConsumer<T>，该接口继承自 IConsumerBase。IConsumer<T> 接口包含一个方法 HandleEventAsync，用于处理事件，接受一个类型为 T 的参数 eventMessage，返回一个表示异步操作的任务。
    
    在 IConsumerBase.cs 文件中添加了 Pek.Events 命名空间，并添加了接口 IConsumerBase。IConsumerBase 接口包含一个属性 Sort，用于排序，类型为 Int32。

---
+++ Pek.Common/Events/IConsumer.cs
@@ -0,0 +1,15 @@
+﻿namespace Pek.Events;
+
+/// <summary>
+/// 消费者接口
+/// </summary>
+/// <typeparam name="T">类型</typeparam>
+public partial interface IConsumer<T> : IConsumerBase
+{
+    /// <summary>
+    /// 处理事件
+    /// </summary>
+    /// <param name="eventMessage">事件</param>
+    /// <returns>一个表示异步操作的任务</returns>
+    Task HandleEventAsync(T eventMessage);
+}
---
+++ Pek.Common/Events/IConsumerBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Pek.Events;
+
+/// <summary>
+/// 消费者基类
+/// </summary>
+public interface IConsumerBase
+{
+    /// <summary>
+    /// 排序
+    /// </summary>
+    Int32 Sort { get; set; }
+}
