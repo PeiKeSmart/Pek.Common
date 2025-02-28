84ddad184006b4d0bccaef824e45b3ec6838685e
猿人易 <qcjxberin@outlook.com>
2025-02-28 20:19:34 +0800

    统一类型命名和简化代码风格
    对方法参数和返回类型进行了统一，使用 `Object` 和 `String` 替代 `object` 和 `string`，并将基本类型更改为相应的包装类型（如 `Byte`、`Int16` 等）。在 `TryParse` 方法中使用相应的类型，简化了代码逻辑。此外，采用 C# 8.0 的模式匹配语法，移除不必要的默认值参数，优化了方法签名。其他方法中也进行了风格一致性的调整。

--- Pek.Common/Helpers/Conv.cs
+++ Pek.Common/Helpers/Conv.cs
@@ -13,22 +13,22 @@ public static partial class Conv
     /// 转换为8位整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static byte ToDGByte(this object input) => ToDGByte(input, default);
+    public static Byte ToDGByte(this Object input) => ToDGByte(input, default);
 
     /// <summary>
     /// 转换为8位整型
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static byte ToDGByte(this object input, byte defaultValue) => ToDGByteOrNull(input) ?? defaultValue;
+    public static Byte ToDGByte(this Object input, Byte defaultValue) => ToDGByteOrNull(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为8位可空整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static byte? ToDGByteOrNull(this object input)
+    public static Byte? ToDGByteOrNull(this Object input)
     {
-        var success = byte.TryParse(input.SafeString(), out var result);
+        var success = Byte.TryParse(input.SafeString(), out var result);
         if (success)
             return result;
         try
@@ -52,14 +52,14 @@ public static partial class Conv
     /// 转换为字节数组
     /// </summary>
     /// <param name="input">输入值</param>        
-    public static byte[] ToDGBytes(this string input) => ToDGBytes(input, Encoding.UTF8);
+    public static Byte[] ToDGBytes(this String input) => ToDGBytes(input, Encoding.UTF8);
 
     /// <summary>
     /// 转换为字节数组
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="encoding">字符编码</param>
-    public static byte[] ToDGBytes(this string input, Encoding encoding) => string.IsNullOrWhiteSpace(input) ? new byte[] { } : encoding.GetBytes(input);
+    public static Byte[] ToDGBytes(this String input, Encoding encoding) => String.IsNullOrWhiteSpace(input) ? [] : encoding.GetBytes(input);
 
     #endregion
 
@@ -69,22 +69,22 @@ public static partial class Conv
     /// 转换为字符
     /// </summary>
     /// <param name="input">输入值</param>
-    public static char ToDGChar(this object input) => ToDGChar(input, default);
+    public static Char ToDGChar(this Object input) => ToDGChar(input, default);
 
     /// <summary>
     /// 转换为字符
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static char ToDGChar(this object input, char defaultValue) => ToDGCharOrNull(input) ?? defaultValue;
+    public static Char ToDGChar(this Object input, Char defaultValue) => ToDGCharOrNull(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为可空字符
     /// </summary>
     /// <param name="input">输入值</param>
-    public static char? ToDGCharOrNull(this object input)
+    public static Char? ToDGCharOrNull(this Object input)
     {
-        var success = char.TryParse(input.SafeString(), out var result);
+        var success = Char.TryParse(input.SafeString(), out var result);
         if (success)
             return result;
         return null;
@@ -98,22 +98,22 @@ public static partial class Conv
     /// 转换为16位整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static short ToDGShort(this object input) => ToDGShort(input, default);
+    public static Int16 ToDGShort(this Object input) => ToDGShort(input, default);
 
     /// <summary>
     /// 转换为16位整型
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static short ToDGShort(this object input, short defaultValue) => ToDGShortOrNull(input) ?? defaultValue;
+    public static Int16 ToDGShort(this Object input, Int16 defaultValue) => ToDGShortOrNull(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为16位可空整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static short? ToDGShortOrNull(this object input)
+    public static Int16? ToDGShortOrNull(this Object input)
     {
-        var success = short.TryParse(input.SafeString(), out var result);
+        var success = Int16.TryParse(input.SafeString(), out var result);
         if (success)
             return result;
         try
@@ -160,7 +160,7 @@ public static partial class Conv
             var temp = ToDGDoubleOrNull(input, 0);
             if (temp == null)
                 return null;
-            return System.Convert.ToInt32(temp);
+            return Convert.ToInt32(temp);
         }
         catch
         {
@@ -176,22 +176,22 @@ public static partial class Conv
     /// 转换为64位整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static long ToDGLong(this object input) => ToDGLong(input, default);
+    public static Int64 ToDGLong(this Object input) => ToDGLong(input, default);
 
     /// <summary>
     /// 转换为64位整型
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static long ToDGLong(this object input, long defaultValue) => ToDGLongOrNull(input) ?? defaultValue;
+    public static Int64 ToDGLong(this Object input, Int64 defaultValue) => ToDGLongOrNull(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为64位可空整型
     /// </summary>
     /// <param name="input">输入值</param>
-    public static long? ToDGLongOrNull(this object input)
+    public static Int64? ToDGLongOrNull(this Object input)
     {
-        var success = long.TryParse(input.SafeString(), out var result);
+        var success = Int64.TryParse(input.SafeString(), out var result);
         if (success)
             return result;
         try
@@ -199,7 +199,7 @@ public static partial class Conv
             var temp = ToDGDecimalOrNull(input, 0);
             if (temp == null)
                 return null;
-            return System.Convert.ToInt64(temp);
+            return Convert.ToInt64(temp);
         }
         catch
         {
@@ -216,7 +216,7 @@ public static partial class Conv
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="digits">小数位数</param>
-    public static float ToDGFloat(this object input, int? digits = null) => ToDGFloat(input, default, digits);
+    public static Single ToDGFloat(this Object input, Int32? digits = null) => ToDGFloat(input, default, digits);
 
     /// <summary>
     /// 转换为32位浮点型，并按指定小数位舍入
@@ -224,21 +224,21 @@ public static partial class Conv
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
     /// <param name="digits">小数位数</param>
-    public static float ToDGFloat(this object input, float defaultValue, int? digits = null) => ToDGFloatOrNull(input, digits) ?? defaultValue;
+    public static Single ToDGFloat(this Object input, Single defaultValue, Int32? digits = null) => ToDGFloatOrNull(input, digits) ?? defaultValue;
 
     /// <summary>
     /// 转换为32位可空浮点型，并按指定小数位舍入
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="digits">小数位数</param>
-    public static float? ToDGFloatOrNull(this object input, int? digits = null)
+    public static Single? ToDGFloatOrNull(this Object input, Int32? digits = null)
     {
-        var success = float.TryParse(input.SafeString(), out var result);
+        var success = Single.TryParse(input.SafeString(), out var result);
         if (!success)
             return null;
         if (digits == null)
             return result;
-        return (float)Math.Round(result, digits.Value);
+        return (Single)Math.Round(result, digits.Value);
     }
 
     #endregion
@@ -250,7 +250,7 @@ public static partial class Conv
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="digits">小数位数</param>
-    public static Double ToDGDouble(this Object input, Int32? digits = null) => ToDGDouble(input, default, digits);
+    public static Double ToDGDouble(this Object? input, Int32? digits = null) => ToDGDouble(input, default, digits);
 
     /// <summary>
     /// 转换为64位浮点型，并按指定小数位舍入，温馨提示：4舍6入5成双
@@ -258,7 +258,7 @@ public static partial class Conv
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
     /// <param name="digits">小数位数</param>
-    public static Double ToDGDouble(this Object input, Double defaultValue, Int32? digits = null) => ToDGDoubleOrNull(input, digits) ?? defaultValue;
+    public static Double ToDGDouble(this Object? input, Double defaultValue, Int32? digits = null) => ToDGDoubleOrNull(input, digits) ?? defaultValue;
 
     /// <summary>
     /// 转换为64位可空浮点型，并按指定小数位舍入，温馨提示：4舍6入5成双
@@ -282,7 +282,7 @@ public static partial class Conv
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="digits">小数位数</param>
-    public static decimal ToDGDecimal(this object input, int? digits = null) => ToDGDecimal(input, default, digits);
+    public static Decimal ToDGDecimal(this Object input, Int32? digits = null) => ToDGDecimal(input, default, digits);
 
     /// <summary>
     /// 转换为128位浮点型，并按指定小数位舍入，温馨提示：4舍6入5成双
@@ -290,16 +290,16 @@ public static partial class Conv
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
     /// <param name="digits">小数位数</param>
-    public static decimal ToDGDecimal(this object input, decimal defaultValue, int? digits = null) => ToDGDecimalOrNull(input, digits) ?? defaultValue;
+    public static Decimal ToDGDecimal(this Object input, Decimal defaultValue, Int32? digits = null) => ToDGDecimalOrNull(input, digits) ?? defaultValue;
 
     /// <summary>
     /// 转换为128位可空浮点型，并按指定小数位舍入，温馨提示：4舍6入5成双
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="digits">小数位数</param>
-    public static decimal? ToDGDecimalOrNull(this object input, int? digits = null)
+    public static Decimal? ToDGDecimalOrNull(this Object input, Int32? digits = null)
     {
-        var success = decimal.TryParse(input.SafeString(), out var result);
+        var success = Decimal.TryParse(input.SafeString(), out var result);
         if (!success)
             return null;
         return digits == null ? result : Math.Round(result, digits.Value);
@@ -313,49 +313,39 @@ public static partial class Conv
     /// 转换为布尔值
     /// </summary>
     /// <param name="input">输入值</param>
-    public static bool ToDGBool(this object input) => ToDGBool(input, default);
+    public static Boolean ToDGBool(this Object input) => ToDGBool(input, default);
 
     /// <summary>
     /// 转换为布尔值
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static bool ToDGBool(this object input, bool defaultValue) => ToDGBoolOrNull(input) ?? defaultValue;
+    public static Boolean ToDGBool(this Object input, Boolean defaultValue) => ToDGBoolOrNull(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为可空布尔值
     /// </summary>
     /// <param name="input">输入值</param>
-    public static bool? ToDGBoolOrNull(this object input)
+    public static Boolean? ToDGBoolOrNull(this Object input)
     {
-        bool? value = GetBool(input);
+        var value = GetBool(input);
         if (value != null)
             return value.Value;
-        return bool.TryParse(input.SafeString(), out var result) ? (bool?)result : null;
+        return Boolean.TryParse(input.SafeString(), out var result) ? (Boolean?)result : null;
     }
 
     /// <summary>
     /// 获取布尔值
     /// </summary>
     /// <param name="input">输入值</param>
-    private static bool? GetBool(object input)
+    private static Boolean? GetBool(Object input)
     {
-        switch (input.SafeString().ToLower())
+        return input.SafeString().ToLower() switch
         {
-            case "0":
-            case "否":
-            case "不":
-            case "no":
-            case "fail":
-                return false;
-            case "1":
-            case "是":
-            case "ok":
-            case "yes":
-                return true;
-            default:
-                return null;
-        }
+            "0" or "否" or "不" or "no" or "fail" => false,
+            "1" or "是" or "ok" or "yes" => true,
+            _ => null,
+        };
     }
 
     #endregion
@@ -366,15 +356,14 @@ public static partial class Conv
     /// 转换为日期
     /// </summary>
     /// <param name="input">输入值</param>
-    /// <param name="defaultValue">默认值</param>
-    public static DateTime ToDGDate(this object input, DateTime defaultValue = default) => ToDGDateOrNull(input) ?? DateTime.MinValue;
+    public static DateTime ToDGDate(this Object input) => ToDGDateOrNull(input) ?? DateTime.MinValue;
 
     /// <summary>
     /// 转换为可空日期
     /// </summary>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static DateTime? ToDGDateOrNull(this object input, DateTime? defaultValue = null)
+    public static DateTime? ToDGDateOrNull(this Object input, DateTime? defaultValue = null)
     {
         if (input == null)
             return defaultValue;
@@ -389,19 +378,19 @@ public static partial class Conv
     /// 转换为Guid
     /// </summary>
     /// <param name="input">输入值</param>
-    public static Guid ToDGGuid(this object input) => ToDGGuidOrNull(input) ?? Guid.Empty;
+    public static Guid ToDGGuid(this Object input) => ToDGGuidOrNull(input) ?? Guid.Empty;
 
     /// <summary>
     /// 转换为可空Guid
     /// </summary>
     /// <param name="input">输入值</param>
-    public static Guid? ToDGGuidOrNull(this object input) => Guid.TryParse(input.SafeString(), out var result) ? (Guid?)result : null;
+    public static Guid? ToDGGuidOrNull(this Object input) => Guid.TryParse(input.SafeString(), out var result) ? (Guid?)result : null;
 
     /// <summary>
     /// 转换为Guid集合
     /// </summary>
     /// <param name="input">输入值，以逗号分隔的Guid集合字符串，范例：83B0233C-A24F-49FD-8083-1337209EBC9A,EAB523C6-2FE7-47BE-89D5-C6D440C3033A</param>
-    public static List<Guid> ToDGGuidList(this string input) => ToDGList<Guid>(input);
+    public static List<Guid> ToDGGuidList(this String input) => ToDGList<Guid>(input);
 
     #endregion
 
@@ -412,13 +401,13 @@ public static partial class Conv
     /// </summary>
     /// <typeparam name="T">目标元素类型</typeparam>
     /// <param name="input">输入值，以逗号分隔的元素集合字符串，范例：83B0233C-A24F-49FD-8083-1337209EBC9A,EAB523C6-2FE7-47BE-89D5-C6D440C3033A</param>
-    public static List<T> ToDGList<T>(this string input)
+    public static List<T> ToDGList<T>(this String input)
     {
         var result = new List<T>();
-        if (string.IsNullOrWhiteSpace(input))
+        if (String.IsNullOrWhiteSpace(input))
             return result;
         var array = input.Split(',');
-        result.AddRange(from each in array where !string.IsNullOrWhiteSpace(each) select To<T>(each));
+        result.AddRange(from each in array where !String.IsNullOrWhiteSpace(each) select To<T>(each));
         return result;
     }
 
@@ -431,7 +420,7 @@ public static partial class Conv
     /// </summary>
     /// <typeparam name="T">枚举类型</typeparam>
     /// <param name="input">输入值</param>
-    public static T ToDGEnum<T>(this object input) where T : struct => ToDGEnum<T>(input, default);
+    public static T ToDGEnum<T>(this Object input) where T : struct => ToDGEnum<T>(input, default);
 
     /// <summary>
     /// 转换为枚举
@@ -439,14 +428,14 @@ public static partial class Conv
     /// <typeparam name="T">枚举类型</typeparam>
     /// <param name="input">输入值</param>
     /// <param name="defaultValue">默认值</param>
-    public static T ToDGEnum<T>(this object input, T defaultValue) where T : struct => ToDGEnumOrNull<T>(input) ?? defaultValue;
+    public static T ToDGEnum<T>(this Object input, T defaultValue) where T : struct => ToDGEnumOrNull<T>(input) ?? defaultValue;
 
     /// <summary>
     /// 转换为可空枚举
     /// </summary>
     /// <typeparam name="T">枚举类型</typeparam>
     /// <param name="input">输入值</param>
-    public static T? ToDGEnumOrNull<T>(this object input) where T : struct
+    public static T? ToDGEnumOrNull<T>(this Object input) where T : struct
     {
         var success = System.Enum.TryParse(input.SafeString(), true, out T result);
         if (success)
@@ -463,11 +452,11 @@ public static partial class Conv
     /// </summary>
     /// <typeparam name="T">目标类型</typeparam>
     /// <param name="input">输入值</param>
-    public static T To<T>(this object input)
+    public static T? To<T>(this Object input)
     {
         if (input == null)
             return default;
-        if (input is string && string.IsNullOrWhiteSpace(input.ToString()))
+        if (input is String && String.IsNullOrWhiteSpace(input.ToString()))
             return default;
 
         var type = Common.GetType<T>();
@@ -475,13 +464,13 @@ public static partial class Conv
         try
         {
             if (typeName == "string")
-                return (T)(object)input.ToString();
+                return (T)(Object)input.ToString()!;
             if (typeName == "guid")
-                return (T)(object)new Guid(input.ToString());
+                return (T)(Object)new Guid(input.ToString()!);
             if (type.IsEnum)
                 return EnumHelper.Parse<T>(input);
             if (input is IConvertible)
-                return (T)System.Convert.ChangeType(input, type);
+                return (T)Convert.ChangeType(input, type);
             return (T)input;
         }
         catch
@@ -498,11 +487,11 @@ public static partial class Conv
     /// </summary>
     /// <param name="Value">对象</param>
     /// <returns>转换后的字符串</returns>
-    public static string ObjToString(this object Value)
+    public static String? ObjToString(this Object Value)
     {
         if (Value.ObjIsNull())
         {
-            return string.Empty;
+            return String.Empty;
         }
         return Value.ToString();
     }
@@ -514,7 +503,7 @@ public static partial class Conv
     /// <param name="DefaultValue">默认值</param>
     /// <param name="Trim">是否去除空格</param>
     /// <returns>转换后的字符串</returns>
-    public static string ObjToString(this object Value, string DefaultValue, bool Trim)
+    public static String? ObjToString(this Object Value, String DefaultValue, Boolean Trim)
     {
         if (Value.ObjIsNull())
         {
@@ -522,7 +511,7 @@ public static partial class Conv
         }
         if (Trim)
         {
-            return Value.ToString().Trim();
+            return Value.ToString()?.Trim();
         }
         return Value.ToString();
     }
@@ -534,7 +523,7 @@ public static partial class Conv
     /// </summary>
     /// <param name="ip">ip</param>
     /// <returns></returns>
-    public static long ConvertIPToLong(this string ip)
+    public static Int64 ConvertIPToLong(this String ip)
     {
         if (!ip.Contains('.'))
         {
@@ -545,7 +534,7 @@ public static partial class Conv
             ip = "127.0.0.1";
         }
         var ips = ip.Split('.');
-        var number = 16777216L * long.Parse(ips[0]) + 65536L * long.Parse(ips[1]) + 256 * long.Parse(ips[2]) + long.Parse(ips[3]);
+        var number = 16777216L * Int64.Parse(ips[0]) + 65536L * Int64.Parse(ips[1]) + 256 * Int64.Parse(ips[2]) + Int64.Parse(ips[3]);
         return number;
     }
 
