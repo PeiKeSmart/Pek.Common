693735dc3175fae3c1f4427a515e720c3f753332
猿人易 <qcjxberin@outlook.com>
2025-04-24 20:44:54 +0800

    新增 AsciiHelper 类及其 HexStringToAsciiString 方法
    新增了一个名为 `AsciiHelper` 的类，包含一个静态方法 `HexStringToAsciiString`。该方法将十六进制字符串转换为 ASCII 字符串，首先将其转换为字节数组，然后筛选出可打印的 ASCII 字符，最后返回去除尾部空格的 ASCII 字符串。

---
+++ Pek.Common/Iot/AsciiHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Pek.Iot;
+
+public class AsciiHelper
+{
+    /// <summary>
+    /// 将十六进制字符串转换为ASCII字符串
+    /// </summary>
+    /// <param name="hexString">十六进制字符串</param>
+    /// <returns></returns>
+    public static String HexStringToAsciiString(String hexString)
+    {
+        var bytes = new Byte[hexString.Length / 2];
+        for (var i = 0; i < hexString.Length; i += 2)
+        {
+            bytes[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+        }
+
+        var asciiChars = new List<Char>();
+        for (var i = 0; i < bytes.Length; i++)
+        {
+            if (bytes[i] >= 32 && bytes[i] <= 126)
+            {
+                asciiChars.Add((Char)bytes[i]);
+            }
+        }
+
+        var asciiString = new String([.. asciiChars]);
+        return asciiString.TrimEnd();
+    }
+}
