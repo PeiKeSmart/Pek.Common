ad979ced0cf6fbef16872bc45b29b35c574d4bd2
猿人易 <qcjxberin@outlook.com>
2025-08-13 15:30:11 +0800

    更新 Common.cs 和 Pek.Common.csproj 文件
    在 `Common.cs` 中添加了 `ToDictionary` 方法，用于将 JSON 数组字符串转换为名值字典，并引入了 `NewLife.Serialization`。同时，更新了 `Pek.Common.csproj` 中的多个 NuGet 包版本，以确保使用最新的功能和修复。

--- Pek.Common/Helpers/Common.cs
+++ Pek.Common/Helpers/Common.cs
@@ -1,4 +1,6 @@
-﻿namespace Pek.Helpers;
+﻿using NewLife.Serialization;
+
+namespace Pek.Helpers;
 
 /// <summary>
 /// 常用公共操作
@@ -20,4 +22,31 @@ public static partial class Common
     public static Type GetType(Type type) => Nullable.GetUnderlyingType(type) ?? type;
 
     #endregion
+
+    /// <summary>Json数组字符串对象转为名值字典</summary>
+    /// <param name="listJson">Json数组字符串对象</param>
+    /// <returns></returns>
+    public static IDictionary<String, Object?> ToDictionary(this String listJson)
+    {
+        var parser = new JsonParser(listJson);
+        var result = parser.Decode();
+
+        var dic = new Dictionary<String, Object?>();
+
+        if (result is List<object> jsonArray)
+        {
+            foreach (var item in jsonArray)
+            {
+                if (item is Dictionary<string, object> dictItem)
+                {
+                    foreach (var kvp in dictItem)
+                    {
+                        dic[kvp.Key] = kvp.Value;
+                    }
+                }
+            }
+        }
+
+        return dic;
+    }
 }
